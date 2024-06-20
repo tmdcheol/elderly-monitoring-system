69a221c1c0737d68b18de5b3e78c380b980c87a3
tmdcheol <leesc08392@gmail.com>
2024-06-21 06:41:45 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/inha/ELDERLYMONITORING/ElderlymonitoringApplication.java
@@ -0,0 +1,13 @@
+package inha.ELDERLYMONITORING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElderlymonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElderlymonitoringApplication.class, args);
+	}
+
+}
